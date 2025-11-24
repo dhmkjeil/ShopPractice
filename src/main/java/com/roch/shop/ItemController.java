885a3e9bc60abf10ddc2c48a58075a5f65d75889
Roch <dhmkjeil@gmail.com>
2025-11-24 11:38:50 +0900

    1. DB에서 상품목록 가져오기  2. 공지사항 페이지 추가 및 DB에서 가져오기

--- src/main/java/com/roch/shop/ItemController.java
+++ src/main/java/com/roch/shop/ItemController.java
@@ -1,14 +1,24 @@
 package com.roch.shop;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+	
+	private final ItemRepository itemRepository;
+	
 	@GetMapping("/list")
 	String itemList(Model model) {
-		model.addAttribute("name", "Kim");
+		List<Item> result = itemRepository.findAll();
+		model.addAttribute("items", result);
+		System.out.println(result.toString());
 		return "itemList.html";
 	}
 }
--- src/main/java/com/roch/shop/Notice.java
+++ src/main/java/com/roch/shop/Notice.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.ToString;
 
 @Entity
+@ToString
 public class Notice {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
